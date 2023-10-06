8df571828ce56257972126f54d79e22bea2e9fae
Kalzud <udumakalz@gmail.com>
2023-10-06 18:54:53 +0100

    v-1.6

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,17 +8,6 @@ import './timeline';
 
 
 
-// app.js
-
-// import { Timeline } from './timeline.js';
-
-// // Now you can use the Timeline class in app.js
-// const timelineContainers = document.querySelectorAll('.timeline-container');
-
-// timelineContainers.forEach((container) => {
-//   const timeline = new Timeline('.timeline', container);
-// });
-
 
 
 
@@ -103,66 +92,3 @@ import './timeline';
 //     // const hiddenElementsRight = document.querySelectorAll('.hiddenRight');
 //     // hiddenElementsRight.forEach((el) => observer.observe(el));
 // }
-
-
-
-// "use strict"; //enables strict mode so as to catch coding mistakes and unsafe actions and throw errors
-
-// //this function would act as a universal function making query selection
-// // easier it would take two arguments the slector(class) as first argument and
-// // then the all which is set to default false as second argument.
-// // if all = false then the function uses querySelector accepting just a single which is the first element with the selector class
-// //but if all = true then the function uses the querySelectorAll to takes mulitple elements on that page that have the slector class
-// function qs(selector, all = false) {
-//   return all ? document.querySelectorAll(selector) : document.querySelector(selector);
-// }
-// //assigning all the selected queries to constants,
-// // note the true in the section qs selects all elements with class section and puts it in a node list
-// const sections = qs('.section', true);
-// const timeline = qs('.timeline');
-// const line = qs('.line', true);
-// line.style.bottom = `calc(100% - 20px)`; //puts line bottom 20 pixels below container bottom
-// let prevScrollY = window.scrollY; //initilise a variable prevScrollY with the current scroll position of the window
-// let up, down; //this variables would be used to decide if the user is scrolling up or down
-// let full = false; //this variable would be used to determine if a scroll distance has been met it is initially set to false
-// let set = 0;//this variable is going to be used to hold a value used in scrolling effect
-// const targetY = window.innerHeight * .8; //this variabl ewould be used for comparison in scroll handler, it is set to 80 percent of window innerheight (visible area on browser)
-
-// function scrollHandler(e) {
-//   const {scrollY} = window; //uses destructing assignment to etract the scrollY (thats the vertical position on page) property from the window
-//   up = scrollY < prevScrollY;// if the scrollY position is less that the prevScrollY (which was the initial scroll position) that would mean the user is scrolling up (remember the y axis starts from 0 and increases downwards)
-//   down = !up;
-//   const timelineRect = timeline.getBoundingClientRect();
-// //   const lineRect = line.getBoundingClientRect(); // const lineHeight = lineRect.bottom - lineRect.top;
-
-//   const dist = targetY - timelineRect.top;
-//   console.log(dist);
-
-
-//     if (down && !full) {
-//         set = Math.max(set, dist);
-//         line.style.bottom = `calc(100% - ${set}px)`;
-//     }
-
-//     if (dist > timeline.offsetHeight + 50 && !full) {
-//         full = true;
-//         line.style.bottom = `-50px`;
-//     }
-
-
-
-//   sections.forEach(item => {
-//     // console.log(item);
-//     const rect = item.getBoundingClientRect(); //     console.log(rect);
-
-//     if (rect.top + item.offsetHeight / 5 < targetY) {
-//       item.classList.add('show-me');
-//     }
-//   }); // console.log(up, down);
-
-//   prevScrollY = window.scrollY;
-// }
-
-// scrollHandler();
-// line.style.display = 'block';
-// window.addEventListener('scroll', scrollHandler);
--- resources/js/timeline.js
+++ resources/js/timeline.js
@@ -1,55 +1,92 @@
-"use strict";
+"use strict"; //enables strict mode so as to catch coding mistakes and unsafe actions and throw errors
 
 class Timeline {
+    // This class takes in two parameters the selector which would be the class of element
+    // (in this case .timeline) and the container of that element (in this case the timeline-container)
   constructor(selector, container) {
+    // here the values to be initialized are assigned and the timeline is
+    // assigned to the selector in a particular container
+    // for example it would take element of class timeline
+    // in a particular element of class timeline-container
     this.timeline = container.querySelector(selector);
+    //then every other variable would now be based on this timeline
     this.line = this.timeline.querySelector('.line');
     this.sections = this.timeline.querySelectorAll('.section');
-    this.prevScrollY = window.scrollY;
-    this.up = false;
-    this.down = false;
-    this.full = false;
-    this.set = 0;
-    this.targetY = window.innerHeight * 0.8;
+    this.prevScrollY = window.scrollY; //initialize a variable prevScrollY with the current vertical scroll position of the window
+    this.up = false; //this variables would be used to decide if the user is scrolling up
+    this.down = false; //this variables would be used to decide if the user is scrolling down
+    this.full = false; //this variable would be used to determine if a scroll distance (full length of .timeline element) has been met it is initially set to false
+    this.set = 0; //this variable is going to be used to hold a value of max scroll value
+    this.targetY = window.innerHeight * 0.8; //this variable would be used for comparison in scroll handler, it is set to 80 percent of window innerheight (visible area on browser)
 
     this.initialize();
   }
 
   initialize() {
-    this.line.style.bottom = `calc(100% - 20px)`;
+    this.line.style.bottom = `calc(100% - 20px)`; //puts line bottom 20 pixels below container bottom
 
-    window.addEventListener('scroll', this.scrollHandler.bind(this));
+    window.addEventListener('scroll', this.scrollHandler.bind(this)); //add scroll event listener to window so it is called when the user scrolls
   }
 
   scrollHandler() {
-    const { scrollY } = window;
+    const { scrollY } = window; //uses destructing assignment to extract the scrollY (thats the vertical position on page) property from the window
+    // ya I know there is no need explaining but i forget sometimes so here it is
+    // the scrollY compares against the this.prevScrollY that was assigned to
+    // the original window vertical scroll position
+    // if it is smaller that means the user has scrolled up cause the
+    // y axis start from 0 and increase downwards so the top is less.
+    // once a scroll up has now been confirmed if the scrollY is less that prevScrollY then
+    // the boolean value of true is assigned to the 'up' variable
+    // ofc if scrollY>prevScrollY then up is false
     this.up = scrollY < this.prevScrollY;
+    // this line is simply assigning the opposite of whatever the up value is to the down
+    // cause ofc the user cannot scroll up and down at the same exact simultaneous same time
+    // (yes I know i used similar words at the same time it's to lay emphasis and avoid confusion)
+    // anyways that means if the user is scrolling up than user is not scrolling down
+    // hence is up is true, down is false and viceversa
     this.down = !this.up;
+    // timelineReact is created with the getBoundingClientRect which gives the dimension
+    // and position of timeline relative to the viewport
     const timelineRect = this.timeline.getBoundingClientRect();
+    //vertical distance between the targetY(we set that to 80% of window innerheight (visible area)) and the top of .timeline element
     const dist = this.targetY - timelineRect.top;
 
+    // this if code block directly below would check if the user is scrolling down (down = true)
+    // and if full is false (we have not yet reached the full length of .timeline element)
+    // if both conditions are passed then we set the 'set' to the max value between it and the dist
+    // that is if Math.max(0, 5) then set is 5. then it goes on to set the line bottom value this.set pixels
+    // below the container bottom making it look like the line goes up as user scrolls down
     if (this.down && !this.full) {
       this.set = Math.max(this.set, dist);
       this.line.style.bottom = `calc(100% - ${this.set}px)`;
     }
 
+    // this code block would check if the dist is greater that the
+    // .timeline.offsetHeight (height of .timeline element)
+    // + 50 pixels (essentially checking if we have gone passed the element)
+    // and if the full is still false. If both conditions are met then we set
+    // full to true saying essentially reached the end of the .timeline element and we move the
+    // line 50 pixels upwards essentially hidding it
     if (dist > this.timeline.offsetHeight + 50 && !this.full) {
       this.full = true;
       this.line.style.bottom = `-50px`;
     }
 
+    //iterates through all elements with .section
     this.sections.forEach((item) => {
+    //calculates its positon relative to viewport using getBoundingClientRect()
       const rect = item.getBoundingClientRect();
+      //checks if top of element + one fifth of height is less than targetY
       if (rect.top + item.offsetHeight / 5 < this.targetY) {
+        //if so adds css styling show-me
         item.classList.add('show-me');
       }
     });
 
-    this.prevScrollY = window.scrollY;
+    this.prevScrollY = window.scrollY; //update the prevScrollY for use on the next scroll
   }
 }
 
-// export { Timeline };
 
 // Initialize each timeline separately
 const timelineContainers = document.querySelectorAll('.timeline-container');
