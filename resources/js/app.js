2df0e6eaade1c49395fd575e54c1e2da5129b7aa
Kalzud <udumakalz@gmail.com>
2023-10-11 19:55:43 +0100

    v-1.11

--- resources/js/app.js
+++ resources/js/app.js
@@ -57,4 +57,27 @@ var typed1 = new Typed(".autotype1", {
 
 
 
+const textList = [
+    'Linkedin: https://www.linkedin.com/in/emmanuel-uduma-781965229/',
+    'Phone Number: +44 (0) 7310881537',
+    'Email: udumakalz@gmail.com',
+    'Github: https://github.com/Kalzud'
+];
+
+const ul = document.getElementById('falling-text');
+
+// Create and append list items with random positions
+textList.forEach((text, index) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+
+    // Randomly position the falling elements
+    li.style.left = Math.floor(Math.random() * window.innerWidth) + 'px';
+
+    ul.appendChild(li);
+});
+
+
+
+
 
